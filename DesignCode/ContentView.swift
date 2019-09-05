33e2c8ef4ae499aba047e0797dffcd8b43654d2c
YangLiang <yangliangsky@gmail.com>
2019-09-05 15:30:09 +0800

    Initial Commit

---
+++ DesignCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by yl on 2019/9/5.
+//  Copyright © 2019 yl. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
