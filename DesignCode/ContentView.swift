7dc66b11b19e0a31829953db5d7d6513a711bab8
YangLiang <yangliangsky@gmail.com>
2019-09-05 18:51:40 +0800

    swiftui 搭界面，配置动画

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -9,13 +9,129 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello World")
+    
+    @State var show = false
+    
+    var body: some View{
+        ZStack {
+            
+            TitleView()
+                .blur(radius: show ? 20 : 0)
+            
+            
+            CardBottomView()
+                .blur(radius: show ? 20 : 0)
+
+            CardView()
+                .background(show ? Color.red :Color("background9"))
+                .cornerRadius(10)
+                .shadow(radius: 20)
+                .offset(x: 0, y: show ? -400 : -40)
+                .scaleEffect(0.85)
+                .rotationEffect(Angle(degrees: show ? 15 : 0))
+//                .rotation3DEffect(Angle(degrees: show ? 50 : 0), axis: (x: 10.0, y: 10.0, z: 10.0))
+                .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.7))
+            
+            CardView()
+                .background(show ? Color.red :Color("background8"))
+                .cornerRadius(10)
+                .shadow(radius: 20)
+                .offset(x: 0, y: show ? -200 : -20)
+                .scaleEffect(0.90)
+                .rotationEffect(Angle(degrees: show ? 10 : 0))
+//                .rotation3DEffect(Angle(degrees: show ? 40 : 0), axis: (x: 10.0, y: 10.0, z: 10.0))
+                .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.5))
+            
+            CertificateView()
+                .scaleEffect(0.95)
+                .rotationEffect(Angle(degrees: show ? 5 : 0))
+//                .rotation3DEffect(Angle(degrees: show ? 30 : 0), axis: (x: 10.0, y: 10.0, z: 10.0))
+                .animation(.spring(response: 0.55, dampingFraction: 1.0, blendDuration: 0.5))
+                .blendMode(.darken)
+                .onTapGesture {
+                    self.show.toggle()
+                }
+                
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            Text("Card back")
+        }
+        .frame(width: 340.0, height:220)
+    }
+}
+
+struct CertificateView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("UI Design")
+                        .font(.headline)
+                        .foregroundColor(Color("accent"))
+                        .padding(.top)
+                    Text("certificate")
+                        .foregroundColor(Color.white)
+                }
+                .padding(.leading)
+                Spacer()
+                Image("Logo")
+                    .resizable()
+                    .frame(width: 30.0, height: 30.0)
+            }
+            .padding(.horizontal)
+            Spacer()
+            Image("Background")
+        }.frame(width: 340.0, height:220)
+            .background(Color.black)
+            .cornerRadius(10)
+            .shadow(radius: 20)
+    }
+}
+
+struct TitleView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Certificates")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                Spacer()
+            }
+            Image("Illustration5")
+            Spacer()
+        }.padding()
+    }
+}
+
+struct CardBottomView: View {
+    var body: some View {
+        VStack(spacing: 20.0) {
+            Rectangle()
+                .frame(width: 60, height: 6)
+                .cornerRadius(3.0)
+                .opacity(0.1)
+            Text("Placeholdesdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasfdasdfsdfsdfasdfasdfasdfasfsfdr")
+            Spacer()
+        }
+        .frame(minWidth:0, maxWidth: .infinity)
+        .padding()
+        .padding(.horizontal)
+        .background(Color.white)
+        .cornerRadius(30)
+        .shadow(radius: 20)
+        .offset(y: 600.0)
     }
 }
