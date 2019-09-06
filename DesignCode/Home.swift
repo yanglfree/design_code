37a2606e0b01f5c654a3f0351c84a647f8cfc289
YangLiang <yangliangsky@gmail.com>
2019-09-06 11:53:33 +0800

    使用数组数据来填充列表

--- DesignCode/Home.swift
+++ DesignCode/Home.swift
@@ -9,13 +9,12 @@
 import SwiftUI
 
 struct Home: View {
+    var menu = menuData
     var body: some View {
-        
         VStack(alignment: .leading, spacing: 30) {
-            MenuRow(image: "person.circle", text: "My Account")
-            MenuRow(image: "creditcard", text: "Billing")
-            MenuRow(image: "person.2", text: "Team")
-            MenuRow(image: "arrow.uturn.down", text: "Sign Out")
+            ForEach(menu){ item in
+                MenuRow(image: item.icon, text: item.title)
+            }
             Spacer()
         }
         .padding(.top, 20)
@@ -51,3 +50,16 @@ struct MenuRow: View {
         }
     }
 }
+
+struct Menu: Identifiable {
+    var id = UUID()
+    var title: String
+    var icon: String
+}
+
+let menuData = [
+    Menu(title: "My Account", icon: "person.circle"),
+    Menu(title: "Billing", icon: "creditcard"),
+    Menu(title: "Team", icon: "person.2"),
+    Menu(title: "Sign Out", icon: "arrow.uturn.down"),
+]
